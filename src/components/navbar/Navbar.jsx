efedbe3f9612347dd5f1017adfd23d968910476b
Utkarsh Srivastava <utkarsh.dev83@gmail.com>
2022-12-16 17:39:29 +0530

    added initial files

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,74 @@
+import React, {useState} from 'react'
+import {RiMenu3Line, RiCloseLin, RiCloseLine } from 'react-icons/ri'
+
+import logo from './logo.svg';
+import './navbar.css'
+//Use () parentheis instead of braces while passing multiple elements using Freagments
+const Menu = () =>(
+  <>
+  <p> <a href="#home">Home </a></p>
+  <p> <a href="#wgpt3">What is GPT3 </a></p>
+  <p> <a href="#possibility">OpenAI </a></p>
+  <p> <a href="#features">CaseStudies</a></p>
+  <p> <a href="#blog">Library </a></p>
+  </>
+)
+
+// Usign BEM -> Block Element MOdifier to name classes
+
+const Navbar = () => {
+
+  const {toggleMenu,setToggleMenu} = useState(false);
+  // this is for to toggle views i.e. desktop to mobile
+
+  return (
+    <div className="gpt3__navbar">
+      <div className='gpt3__navbar-links'>
+        <div className='gpt3__navbar-links_logo'>
+            <img src={logo} alt="logo" />
+        </div>
+          {/* import all assetas now */}
+        <div className="gtp3__navbar-links_container">
+
+          <Menu />
+
+        </div>
+      </div>
+
+      <div className='gpt3__navbar-sign'>
+
+        <p>Sign in</p>
+        <button type='button'> Sign up</button>
+
+      </div>
+      {/* to add a mobile menu */}
+      <div className='gpt3__navbar-menu'>
+
+          {toggleMenu
+          ? <RiCloseLine color='#fff' size={27} onClick={()=> setToggleMenu(false)} /> 
+          :<RiMenu3Line color='#fff' size={27} onClick={()=> setToggleMenu(true)} /> 
+          } 
+          
+          {/* if toggleMenu is true only then render the following component */}
+          {toggleMenu && (
+            <div className='gpt3__navbar-menu_container scale-up-center'>
+              <div className='gpt3__navbar-menu_container'>
+
+                  <Menu />
+                  <div className='gpt3__navbar-menu_container-links-sign'>
+
+                    <p>Sign in</p>
+                    <button type='button'> Sign up</button>
+
+                  </div>
+              </div>
+            </div>
+          )} 
+          
+
+      </div>
+    </div>
+  )
+}
+
+export default Navbar;
---
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './whatGPT3.css'
+const WHATGPT3 = () => {
+  return (
+
+    <div>WHATGPT3</div>
+  
+    )
+}
+
+export default WHATGPT3
